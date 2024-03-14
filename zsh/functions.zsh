eee797ebc673a1f3ebca1f6bfe5650a8600729c3
Danny DeBelius <danny.debelius@gmail.com>
2024-03-14 17:43:18 -0600

    Fix edit minDuration

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -57,14 +57,11 @@ function json() {
 	fi
 }
 
-
 # take this repo and copy it to somewhere else minus the .git stuff.
 function gitexport(){
 	mkdir -p "$1"
 	git archive master | tar -x -C "$1"
 }
-
-
 
 # get gzipped size
 function gz() {
