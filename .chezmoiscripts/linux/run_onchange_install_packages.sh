83826d2541206e3d06d20ac1c8b2fcded3833493
Danny DeBelius <danny.debelius@gmail.com>
2024-03-14 13:57:05 -0600

    Run linux packages on change

--- .chezmoiscripts/linux/run_onchange_install_packages.sh
+++ .chezmoiscripts/linux/run_onchange_install_packages.sh
@@ -5,8 +5,7 @@ set -eu
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   CI=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/ubuntu/.zshrc
-	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	PATH=/home/linuxbrew/.linuxbrew/bin:$PATH:
 else 
 	echo "Homebrew already installed."
 fi
